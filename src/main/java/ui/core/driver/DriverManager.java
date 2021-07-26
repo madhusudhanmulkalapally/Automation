b71adf89e557c61d0139418c70b3cddbf790af99
madhusudhan <madhus.te7@gmail.com>
2021-07-26 14:02:07 +0530

    xml file

--- src/main/java/ui/core/driver/DriverManager.java
+++ src/main/java/ui/core/driver/DriverManager.java
@@ -11,6 +11,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DriverManager {
+    public static final String USERNAME = "oauth-madhus.te7-aeaf9";
+    public static final String ACCESS_KEY = "44a7e556-4493-4bbb-845b-c3b3391c3637";
+    public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
 
     private static final String HEADLESS = "headless";
     public static WebDriver createInstance(String browserName, String appUrl, String methodName)throws MalformedURLException  {
@@ -33,6 +36,17 @@ public class DriverManager {
                 DesiredCapabilities cap = DesiredCapabilities.chrome();
                 cap.setCapability("name", methodName);
                 driver = new RemoteWebDriver(new URL("http://"+gridId+":80/wd/hub"), cap);
+            }
+            if(browserName.toLowerCase().contains("sauce")) {
+                DesiredCapabilities caps = DesiredCapabilities.chrome();
+                caps.setCapability("platform", "Windows 10");
+                caps.setCapability("version", "latest");
+                caps.setCapability("name", "Test1");
+                caps.setCapability("extendedDebugging", "true");
+                //caps.setCapability("tunnelIdentifier", "be60954fd81249779be34b58c67fd1bd");
+                System.out.println("sauce lab execution started");
+
+                driver = new RemoteWebDriver(new URL(URL), caps);
             }
             driver.navigate().to("https://www.google.com");
             return driver;
